cd7deeccb45c4ce23e8e4d36f5001c6b00047c4a
Matthew Collier <collier.matthew@gmail.com>
2016-01-03 13:00:37 -0500

    Work in bedrock integration.

--- configs/dev.config.js
+++ configs/dev.config.js
@@ -1,8 +1,8 @@
 var config = require('bedrock').config;
 
 // server info
-config.server.port = 3443;
-config.server.httpPort = 3000;
+config.server.port = 3000;
+config.server.httpPort = 3080;
 config.server.bindAddr = ['localhost'];
 config.server.domain = '127.0.0.1';
 config.server.host = '127.0.0.1:' + config.server.httpPort;
